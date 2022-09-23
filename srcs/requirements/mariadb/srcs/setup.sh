91682e115b0d49510ca5c86ab75895264b240eeb
seipan1213 <seiju1213@gmail.com>
2022-09-23 16:50:42 +0900

    mariadb ほぼ設定完了

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+export MYSQL_ROOT_PASS=toor
+export WP_DB_NAME=hogeDB
+export WP_DB_USER=hogeUser
+export WP_DB_USER_PASS=hogePass
 
 /etc/init.d/mariadb setup
 rc-status
 rc-service mariadb start
-mysql -e "CREATE DATABASE wpdb;"
-mysql -e "CREATE USER 'root'@'%' identified by 'toor';"
-mysql -e "GRANT ALL PRIVILEGES ON wpdb.* TO 'root'@'%';"
+
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';";
+mysql -e "CREATE DATABASE ${WP_DB_NAME};"
+mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_USER_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
+rc-service mariadb stop
+
+mysqld_safe
