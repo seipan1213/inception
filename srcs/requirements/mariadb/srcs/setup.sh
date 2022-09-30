14c14ef8439b88551e641853074df69ac8397cd5
seipan1213 <seiju1213@gmail.com>
2022-09-30 21:05:25 +0900

    volume 追加

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -4,11 +4,13 @@
 rc-status
 rc-service mariadb start
 
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';";
+
 mysql -e "CREATE DATABASE ${WP_DB_NAME};"
-mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_USER_PASS}';"
+mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';";
+
 rc-service mariadb stop
 
 mysqld_safe
--- srcs/requirements/wordpress/srcs/setup.sh
+++ srcs/requirements/wordpress/srcs/setup.sh
@@ -2,7 +2,7 @@
 
 wp core download --locale=ja
 
-wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_USER_PASS} --dbhost=${WP_DB_HOST} --dbprefix=wordpress_
+wp config create --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS} --dbhost=${WP_DB_HOST}
 
 wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_USER_PASS} --admin_email=${WP_ADMIN_USER_EMAIL}
 
