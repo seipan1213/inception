075d9d9640b8c7f7e5773c13f1cdd2df522f425f
seipan1213 <seiju1213@gmail.com>
2022-09-30 19:00:02 +0900

    コンテナ連携途中

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ docker build -t sehattor/nxing:v1 ./srcs/requirements/nginx/
 docker build -t sehattor/wordpress:v1 ./srcs/requirements/wordpress/
 docker build -t sehattor/mariadb:v1 ./srcs/requirements/mariadb/
 
-docker-compose -f ./srcs/docker-compose.yml up -d
+docker compose -f ./srcs/docker-compose.yml up -d
