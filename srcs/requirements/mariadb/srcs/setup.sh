8c4e23979967961d17cae0b43c9d82c654f6f70f
seipan1213 <seiju1213@gmail.com>
2022-09-30 22:02:06 +0900

    wordpress nginx 連携

--- srcs/requirements/mariadb/srcs/setup.sh
+++ srcs/requirements/mariadb/srcs/setup.sh
@@ -9,7 +9,7 @@ mysql -e "CREATE DATABASE ${WP_DB_NAME};"
 mysql -e "CREATE USER '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';";
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASS}';";
 
 rc-service mariadb stop
 
